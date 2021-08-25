44e3aeae502482561d206eeb41a8237f23c889cf
Olivier Boudeville <olivier.boudeville@esperide.com>
2021-08-25 19:09:16 +0200

    Even more brutal kill useful.

--- priv/bin/kill-us-main.sh
+++ priv/bin/kill-us-main.sh
@@ -3,7 +3,10 @@
 # Of course using stop-us-main-{native-build,release}.sh shall be preferred:
 echo " Killing brutally (not stopping gracefully) any US-Main instance found, and EPMD as well..."
 
-kill $(ps -edf | grep us_main | grep -v run_erl | grep -v grep | awk '{ print $2 }') 2>/dev/null
+
+# The signal 9 *is* necessary in some cases:
+kill -9 $(ps -edf | grep us_main | grep -v run_erl | grep -v grep | awk '{ print $2 }') 2>/dev/null
+
 killall epmd
 
 sleep 1
