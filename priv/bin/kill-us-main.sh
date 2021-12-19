48cb4483aeaf8444b927b7e0912ecfece2b95c36
Olivier Boudeville <olivier.boudeville@esperide.com>
2021-12-19 21:21:10 +0100

    Better slaughtering.

--- priv/bin/kill-us-main.sh
+++ priv/bin/kill-us-main.sh
@@ -4,10 +4,21 @@
 echo " Killing brutally (not stopping gracefully) any US-Main instance found, and EPMD as well..."
 
 
-# The signal 9 *is* necessary in some cases:
-kill -9 $(ps -edf | grep us_main | grep -v run_erl | grep -v grep | awk '{ print $2 }') 2>/dev/null
+to_kill="$(ps -edf | grep us_main | grep -v run_erl | grep -v grep | awk '{ print $2 }')"
 
-killall epmd
+if [ -n "${to_kill}" ]; then
+
+	echo "Following US-Main processes to kill found: ${to_kill}."
+	# The signal 9 *is* necessary in some cases:
+	kill -9 ${to_kill} # 2>/dev/null
+
+else
+
+	echo "(no US-Main process to kill found)"
+
+fi
+
+killall -9 epmd
 
 sleep 1
 
