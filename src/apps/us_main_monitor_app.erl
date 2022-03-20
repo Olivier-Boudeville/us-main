a9b7acfa62ad77d472af5ff6a1dc09574cf59457
Olivier Boudeville <olivier.boudeville@esperide.com>
2022-03-20 12:39:43 +0100

    exec/0 API updated.

--- src/apps/us_main_monitor_app.erl
+++ src/apps/us_main_monitor_app.erl
@@ -1,4 +1,4 @@
-% Copyright (C) 2021-2022 Olivier Boudeville
+% Copyright (C) 2020-2022 Olivier Boudeville
 %
 % This file belongs to the US-Main project, a part of the Universal Server
 % framework.
@@ -20,7 +20,6 @@
 % Creation date: Sunday, January 19, 2020.
 
 
-
 % @doc Actual US-Main <b>trace monitoring logic</b>, as a (Myriad) application.
 %
 % Typically called through the us_main/priv/bin/monitor-us-main.sh script.
@@ -30,12 +29,12 @@
 %
 -module(us_main_monitor_app).
 
-% For exec/0 export:
--include_lib("myriad/include/app_facilities.hrl").
+
+-export([ exec/0 ]).
+
 
 % For update_code_path_for_myriad/0 and all:
 -include_lib("myriad/include/myriad_script_include.hrl").
-
 
 
 % For trace_aggregator_name:
@@ -163,7 +162,7 @@ init_from_command_line() ->
 	ArgTable = shell_utils:get_argument_table(),
 
 	%trace_utils:debug_fmt( "Argument table: ~ts",
-	%						[ list_table:to_string( ArgTable ) ] ),
+	%                       [ list_table:to_string( ArgTable ) ] ),
 
 	% Argument expected to be set by the caller script:
 	{ CfgFilePath, ConfigShrunkTable } =
