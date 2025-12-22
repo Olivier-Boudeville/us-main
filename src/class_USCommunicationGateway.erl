630dd86a3594c663f0ddcd18fcfa41d493c9e220
Olivier Boudeville <olivier.boudeville@esperide.com>
2025-12-22 22:46:40 +0100

    Notify by SMS added.

--- src/class_USCommunicationGateway.erl
+++ src/class_USCommunicationGateway.erl
@@ -81,7 +81,9 @@ Such gateways rely on a contact directory.
 
 
 -doc "Settings gathered for the communication server.".
--type communication_settings() :: [ bin_phone_number() ]. % MasterBinNumbers
+-type communication_settings() ::
+    { NotifiedBinNumbers :: [ bin_phone_number() ],
+      MasterBinNumbers :: [ bin_phone_number() ] }.
 
 
 -doc "The PID of a communication gateway.".
@@ -111,7 +113,8 @@ Such gateways rely on a contact directory.
 
 % Entries in the US-configuration files for communication:
 
--define( us_main_master_numbers_key, communication_mobile_master_numbers ).
+-define( us_main_notified_numbers_key, communication_mobile_notified_numbers ).
+-define( us_main_master_numbers_key,   communication_mobile_master_numbers ).
 
 
 
@@ -134,6 +137,9 @@ Such gateways rely on a contact directory.
 
     { sms_polling_max_delay, polling_delay(), "the maximum delay between two "
       "pollings of the SMSs" },
+
+    { notified_phone_numbers, [ bin_phone_number() ], "the phone numbers that, "
+      "through SMSs, shall be notified of the major server-side events" },
 
     { master_phone_numbers, [ bin_phone_number() ], "the phone numbers that, "
       "through SMSs, can control the overall US-Main instance" },
@@ -178,6 +184,8 @@ Such gateways rely on a contact directory.
 % Type shorthands:
 
 -type ustring() :: text_utils:ustring().
+-type any_string() :: text_utils:any_string().
+
 -type trace_format() :: text_utils:trace_format().
 -type trace_values() :: text_utils:trace_values().
 
@@ -283,10 +291,11 @@ construct( State ) ->
     InitCommState = init_communications( SrvState ),
 
     % Interleaving of call to getCommunicationSettings/1:
-    MasterBinPhoneNumbers = receive
+    { NotifiedBinPhoneNumbers, MasterBinPhoneNumbers } = receive
 
         % Already checked by the US-Main configuration server to be a list:
-        { wooper_result, CommSettings } when is_list( CommSettings ) ->
+        { wooper_result,
+          CommSettings={ _NotifiedBinPhoneNumbers, _MasterBinPhoneNumbers } } ->
             CommSettings
 
     end,
@@ -296,21 +305,37 @@ construct( State ) ->
         { sms_polling_delay, ?sms_polling_max_delay }, % Start nominal
         { sms_polling_min_delay, ?sms_polling_min_delay },
         { sms_polling_max_delay, ?sms_polling_max_delay },
+        { notified_phone_numbers, NotifiedBinPhoneNumbers },
         { master_phone_numbers, MasterBinPhoneNumbers },
         { parent_comm_gateway_lookup_info, undefined } ] ),
 
     % Starts the SMS polling mechanism if relevant:
     getAttribute( SetState, sms_support_active ) =:= true andalso
+         case NotifiedBinPhoneNumbers of
+
+            [] ->
+                ?send_notice( SetState, "SMS support active, yet with "
+                    "no phone number to notify defined; SMS-based "
+                    "server-side notifications disabled." );
+
+            _ ->
+                ?send_info_fmt( SetState, "SMS support active, with "
+                    "on the following phone numbers to notify: ~ts.",
+                    [ text_utils:strings_to_listed_string(
+                        NotifiedBinPhoneNumbers ) ] )
+
+        end,
+
          case MasterBinPhoneNumbers of
 
             [] ->
-                ?send_warning( SetState, "SMS support active, yet with "
+                ?send_notice( SetState, "SMS support active, yet with "
                     "no master phone number defined; SMS-based "
                     "actions disabled." );
 
-            MasterBinPhoneNumbers ->
-                ?send_info_fmt( SetState, "SMS support active, based "
-                    "on the following master phone numbers: ~ts.",
+            _ ->
+                ?send_info_fmt( SetState, "SMS support active, with "
+                    "the following master phone numbers defined: ~ts.",
                     [ text_utils:strings_to_listed_string(
                         MasterBinPhoneNumbers ) ] ),
                  self() ! pollSMS
@@ -336,7 +361,61 @@ destruct( State ) ->
 
 
 
+
 % Method section.
+
+
+-doc "Notifies by SMS the recipients of interest of the specified message.".
+-spec notifyBySMS( wooper:state(), any_string() ) -> const_oneway_return().
+notifyBySMS( State, Msg ) ->
+
+    case ?getAttr(sms_support_active) of
+
+        true ->
+            case ?getAttr(notified_phone_numbers) of
+
+                [] ->
+                    cond_utils:if_defined( us_main_debug_sms_communication,
+                        ?debug_fmt( "No registered phone numbers to be "
+                                    "notified of '~ts'.", [ Msg ] ), ok );
+
+                RecipientPhoneNumbers ->
+
+                    cond_utils:if_defined( us_main_debug_sms_communication,
+                        ?debug_fmt( "Sending a SMS notification to ~p, "
+                            "for the following message:~n ~ts",
+                            [ RecipientPhoneNumbers, BinMsg ] ) ),
+
+                    Reports = mobile:send_sms_multi( Msg,
+                                                     RecipientPhoneNumbers ),
+
+                    case mobile:detect_failed_sendings( Reports,
+                            RecipientPhoneNumbers ) of
+
+                        [] ->
+                            ok;
+
+                        FailedPhoneNumbers ->
+                            ?error_fmt( "The sending of the SMS notification "
+                                "'~ts' failed for the following recipient "
+                                "mobile phone numbers: ~ts.", [ Msg,
+                                    text_utils:strings_to_listed_string(
+                                        FailedPhoneNumbers ) ] )
+
+                    end
+
+            end;
+
+
+        false ->
+            cond_utils:if_defined( us_main_debug_sms_communication,
+                ?debug_fmt( "No active SMS support, notification '~ts' "
+                            "not sent.", [ Msg ] ), ok )
+
+    end,
+
+    wooper:const_return().
+
 
 
 -doc "Polls adaptatively any incoming SMSs, to trigger associated actions.".
@@ -434,8 +513,8 @@ process_sms_messages( MasterBinPhoneNumbers, State ) ->
 
 
 -spec filter_sms_messages( [ received_sms() ], boolean(),
-        [ bin_phone_number() ], wooper:state() ) ->
-            { boolean(), wooper:state() }.
+                           [ bin_phone_number() ], wooper:state() ) ->
+                                        { boolean(), wooper:state() }.
 filter_sms_messages( _SMSs=[], RelevantSMSFound, _MasterBinPhoneNumbers,
                      State ) ->
     { RelevantSMSFound, State };
@@ -678,7 +757,7 @@ Returns the known sensor-related keys in the US-Main configuration files.
 """.
 -spec get_licit_config_keys() -> [ list_table:key() ].
 get_licit_config_keys() ->
-    [ ?us_main_master_numbers_key ].
+    [ ?us_main_notified_numbers_key, ?us_main_master_numbers_key ].
 
 
 
@@ -693,43 +772,82 @@ it calls this function from its context.
                                         wooper:state().
 manage_configuration( ConfigTable, State ) ->
 
-    % First checking:
+    % First checkings:
+
+    NotifiedBinNumbers = case table:lookup_entry( ?us_main_notified_numbers_key,
+                                                ConfigTable ) of
+
+        key_not_found ->
+            send_comm_trace( info, "No phone number to notify has been "
+                "defined, so no server-side event notification will be sent "
+                "by SMS.", State ),
+            [];
+
+        { value, NotifiedNumbers } when is_list( NotifiedNumbers ) ->
+            NotifBinNums = [ sms_utils:check_phone_number( PN )
+                        || PN <- NotifiedNumbers ],
+            case NotifBinNums of
+
+                [] ->
+                    send_comm_trace( info, "No phone number to notify "
+                        "has been set, so no server-side event notification "
+                        "by SMS will be sent by SMS.", State );
+
+                [ NotifBinNum ] ->
+                    send_comm_trace_fmt( info, "A single phone number to "
+                        "notify has been set: ~ts; it will thus receive "
+                        "SMS-based server-side event notifications.",
+                        [ NotifBinNum ], State );
+
+                _ ->
+                    send_comm_trace_fmt( info,"~B phone numbers to notify "
+                        "have been set: ~ts; they will thus receive "
+                        "SMS-based server-side event notifications.",
+                        [ length( NotifBinNums ),
+                          text_utils:strings_to_listed_string( NotifBinNums ) ],
+                        State )
+
+            end,
+            NotifBinNums
+
+    end,
+
     MasterBinNumbers = case table:lookup_entry( ?us_main_master_numbers_key,
                                                 ConfigTable ) of
 
         key_not_found ->
-            ?info( "No master phone number defined, so no control by SMS will "
-                   "be offered." ),
+            send_comm_trace( info, "No master phone number has been defined, "
+                "so no control by SMS will be granted.", State ),
             [];
 
         { value, MasterNumbers } when is_list( MasterNumbers ) ->
-            BinNums = [ sms_utils:check_phone_number( PN )
+            MstBinNums = [ sms_utils:check_phone_number( PN )
                         || PN <- MasterNumbers ],
-            case BinNums of
+            case MstBinNums of
 
                 [] ->
                     send_comm_trace( info,
-                        "No master phone number set, so no control by SMS "
-                        "will be offered.", State );
+                        "No master phone number has been set, so no control "
+                        "by SMS will be granted.", State );
 
-                [ BinNum ] ->
+                [ MstBinNum ] ->
                     send_comm_trace_fmt( info, "A single master phone number "
-                        "set: ~ts; it will provide control by SMS.", [ BinNum ],
-                        State );
+                        "has been set: ~ts; it will provide control by SMS.",
+                        [ MstBinNum ], State );
 
                 _ ->
-                    send_comm_trace_fmt( info,"~B master phone numbers "
-                        "set: ~ts; they will provide control by SMS.",
-                        [ length( BinNums ),
-                          text_utils:strings_to_listed_string( BinNums ) ],
+                    send_comm_trace_fmt( info,"~B master phone numbers have "
+                        "been set: ~ts; they will provide control by SMS.",
+                        [ length( MstBinNums ),
+                          text_utils:strings_to_listed_string( MstBinNums ) ],
                         State )
 
             end,
-            BinNums
+            MstBinNums
 
     end,
 
-    CommSettings = MasterBinNumbers,
+    CommSettings = { NotifiedBinNumbers, MasterBinNumbers },
 
     setAttribute( State, communication_settings, CommSettings ).
 
@@ -765,14 +883,31 @@ to_string( State ) ->
     SMSStr = case ?getAttr(sms_support_active) of
 
         true ->
+            NotifiedStr = case ?getAttr(notified_phone_numbers) of
+
+                [] ->
+                    "no phone number to notify is set";
+
+                NotifL=[ _SingleNPNBinStr ] ->
+                    text_utils:format(
+                        "a single phone number to notify is set, ~ts", NotifL );
+
+                NPNBinStrs ->
+                    text_utils:format(
+                        "~B phone numbers to notify are set, ~ts",
+                        [ length( NPNBinStrs ),
+                          text_utils:strings_to_listed_string( NPNBinStrs ) ] )
+
+            end,
+
             MasterStr = case ?getAttr(master_phone_numbers) of
 
                 [] ->
                     "no master phone number is set";
 
-                L=[ _SingleMPNBinStr ] ->
+                MstL=[ _SingleMPNBinStr ] ->
                     text_utils:format( "a single master phone number is set, "
-                                       "~ts", L );
+                                       "~ts", MstL );
 
                 MPNBinStrs ->
                     text_utils:format( "~B master phone numbers are set, ~ts",
@@ -782,12 +917,13 @@ to_string( State ) ->
             end,
 
             text_utils:format( "an active SMS support (current polling delay: "
-                "~ts; min: ~ts / max: ~ts) for which ~ts",
+                "~ts; min: ~ts / max: ~ts) for which ~ts and ~ts",
                 [ time_utils:duration_to_string( ?getAttr(sms_polling_delay) ),
                   time_utils:duration_to_string(
                     ?getAttr(sms_polling_min_delay) ),
                   time_utils:duration_to_string(
-                    ?getAttr(sms_polling_max_delay) ), MasterStr ] );
+                    ?getAttr(sms_polling_max_delay) ),
+                  NotifiedStr, MasterStr ] );
 
         false ->
             "no active SMS support"
