1a5b4b667c6badc96cd33a921d6620ce22247f4b
Olivier Boudeville <olivier.boudeville@esperide.com>
2025-12-26 17:37:20 +0100

    Copyright update for 2026.

--- include/class_USMainCentralServer.hrl
+++ include/class_USMainCentralServer.hrl
@@ -1,4 +1,4 @@
-% Copyright (C) 2021-2025 Olivier Boudeville
+% Copyright (C) 2021-2026 Olivier Boudeville
 %
 % This file belongs to the US-Main project, a part of the Universal Server
 % framework.
